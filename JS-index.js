5f52241135a6c79816b95449813a03fb4f05d433
waseem-aziz-kasmani <154504249+waseem-aziz-kasmani@users.noreply.github.com>
2024-04-27 12:12:07 +0500

    Update JS-index.js

--- JS-index.js
+++ JS-index.js
@@ -1,8 +1,23 @@
 function Click1(){  
-  
-  
+
+let c1 = document.getElementById("CN1");
+
+if (c1.style.display == "none"){
+  c1.style.display  = "block";
+}else{
+c1.style.display  = "none";
+}
 }
 
 function Click2(){
- 
+let c1 = document.getElementById("CN2");
+if (c1.style.display == "none"){
+  c1.style.display  = "block";
+}else{
+c1.style.display  = "none";
+}
+}
+
+function Open_TankCal(){
+open("https://github.com/waseem-aziz-kasmani/GP/blob/main/HTML-Tank.html"
 }
