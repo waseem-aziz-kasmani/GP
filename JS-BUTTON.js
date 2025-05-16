069f838ddf266b0112ccf7cf57d4ff5451d34607
Waseem Kasmani <154504249+waseem-aziz-kasmani@users.noreply.github.com>
2025-05-16 17:17:39 +0500

    Create JS-BUTTON.js

---
+++ JS-BUTTON.js
@@ -0,0 +1,49 @@
+function MButton1(){  
+    var x = document.getElementById("BX1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function MButton2(){  
+    var x = document.getElementById("BX2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function Button1(){  
+    var x = document.getElementById("D2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function Button2(){  
+     var x = document.getElementById("D3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }    
+}
+function ImportText(){  
+     var x = document.getElementById("TX1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }    
+}
+function SaveText(){  
+     var x = document.getElementById("TX2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }    
+}
