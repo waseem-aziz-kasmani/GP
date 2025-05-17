8c32c3fc1af4829dd1a3b4a699bcff696ef9013c
Waseem Kasmani <154504249+waseem-aziz-kasmani@users.noreply.github.com>
2025-05-17 10:49:25 +0500

    Create Write_file_1_JS.js

---
+++ Write_file_1_JS.js
@@ -0,0 +1,13 @@
+function SaveFile1Text(){
+   const fs = require('fs');
+    
+    const content = 'Hello, world!';
+    
+    fs.writeFile('output.txt', content, (err) => {
+      if (err) {
+        console.error('Error writing to the file:', err);
+        return;
+      }
+      console.log('File written successfully!');
+    });
+}
