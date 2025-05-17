e8903cd5e2ff852c3ecf366c64aaae591fecfbad
Waseem Kasmani <154504249+waseem-aziz-kasmani@users.noreply.github.com>
2025-05-17 11:17:21 +0500

    Update Write_file_1_JS.js

--- Write_file_1_JS.js
+++ Write_file_1_JS.js
@@ -3,7 +3,7 @@ function SaveFile1Text(){
     
     const content = 'Hello, world!';
     
-    fs.writeFile('output.txt', content, (err) => {
+    fs.writeFile('File_1.txt', content, (err) => {
       if (err) {
         console.error('Error writing to the file:', err);
         return;
